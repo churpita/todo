ebf6aa84511a021e363755387fe455a702d0b7df
Anthony Churpita <32397472+churpita@users.noreply.github.com>
2023-04-02 10:51:26 -0400

    Move taskData state into GroupList

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,6 +1,4 @@
-import React, { useEffect, useState } from "react";
-
-import { TailSpin } from 'react-loader-spinner';
+import React, { useState } from "react";
 
 import Header from './Header';
 import GroupList from "./GroupList";
@@ -9,83 +7,16 @@ import './App.css';
 
 const App = () => {
     const [theme, setTheme] = useState("app-theme-dark");
-    const [loading, setLoading] = useState(true);
-    const [fetchErrorMessage, setFetchErrorMessage] = useState(null);
-    const [taskData, setTaskData] = useState({
-        statusMessage: null,
-        content: {
-            groups: [],
-            members: []
-        }
-    })
 
     const themeToggler = () => {
         setTheme((curr) => (curr === "app-theme-dark" ? "app-theme-light" : "app-theme-dark"));
     }
 
-    const addGroupHandler = (title, color) => {
-        // First, calculate the max task_group_key, and increment it to create the new record's key
-        const newTaskGroupKey = Math.max.apply(Math, taskData.content.groups.map(group => group.task_group_key)) + 1;
-        const newTaskGroup = {
-            task_group_key: newTaskGroupKey,
-            title: title,
-            color: color
-        }
-        
-        //console.log(`addGroupHandler called in App.js\nkey: ${newTaskGroupKey}\ntitle: ${title}\ncolor: ${color}`);
-        
-        // Attempt to post the new group to the API
-        //      If successful, update the state accordingly
-        //      If unsuccessful, throw an error message and don't make any further changes
-
-        setTaskData(prev => {
-            // First add the new group into the groups array
-            const updatedGroups = [...prev.content.groups, newTaskGroup];
-            // Then merge that updated groups array into the new content array
-            const updatedContent = {groups: updatedGroups, members: prev.content.members};
-            // And finally, push the new content array to the new state of taskData
-            return {statusMessage: prev.statusMessage, content: updatedContent};
-        });
-    }
-
-    async function fetchTaskData() {
-        console.log('Fetching tasks from API');
-        try {
-            const taskDataApiResponse = await fetch(`${process.env.REACT_APP_API_URL}/groups`);
-            const taskDataApiJson = await taskDataApiResponse.json();
-
-            if (taskDataApiJson.statusMessage) {
-                setFetchErrorMessage(taskDataApiJson.statusMessage);
-                setLoading(false);
-            }
-            else {
-                setTaskData(taskDataApiJson);
-                setLoading(false);
-            }
-        }
-        catch (err) {
-            console.log(err);
-            setFetchErrorMessage("An error occurred when fetching the task data.");
-            setLoading(false);
-        }
-    }
-
-    useEffect(() => {
-        fetchTaskData();
-    }, []);
-
     return (
         <div className={`app-container ${theme}`}>
-            <Header toggleTheme={themeToggler} addGroup={addGroupHandler} />
+            <Header toggleTheme={themeToggler} />
             <div className="body">
-                {/* Display error message */}
-                {fetchErrorMessage && !loading && <div>{fetchErrorMessage}</div>}
-
-                {/* Display loading screen */}
-                {!fetchErrorMessage && loading && <TailSpin wrapperClass="loadingSpinner" />}
-
-                {/* Display group list after data has been fetched */}
-                {!fetchErrorMessage && !loading && <GroupList data={taskData.content} />}
+                <GroupList />
             </div>
         </div>
     );
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,16 +2,13 @@ import React from "react";
 
 import IconButton from "./IconButton";
 
-import { MdAddCircleOutline, MdDarkMode } from "react-icons/md";
+import { MdDarkMode } from "react-icons/md";
 
 import styles from './Header.module.css';
 
 const Header = props => {
     return (
         <header className={styles.header}>
-            <IconButton>
-                <MdAddCircleOutline onClick={e => props.addGroup("School Tasks", '2bb32b')} size={"2em"} />
-            </IconButton>
             <IconButton>
                 <MdDarkMode onClick={props.toggleTheme} size={"2em"} />
             </IconButton>
