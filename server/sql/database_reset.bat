c8c81717b0e42700867ce2cb4836b095931e18e2
Anthony Churpita <32397472+churpita@users.noreply.github.com>
2023-04-15 13:29:57 -0400

    SQL SP to delete task groups

--- server/sql/database_reset.bat
+++ server/sql/database_reset.bat
@@ -3,3 +3,4 @@ mysql --user=root --password=password --database=todo < script_drop_tables.sql
 mysql --user=root --password=password --database=todo < table_task.sql
 mysql --user=root --password=password --database=todo < table_task_group.sql
 mysql --user=root --password=password --database=todo < table_task_group_member.sql
+mysql --user=root --password=password --database=todo < sp_delete_task_group.sql
---
+++ server/sql/sp_delete_task_group.sql
@@ -0,0 +1,29 @@
+DROP PROCEDURE IF EXISTS delete_task_group;
+DELIMITER //
+CREATE PROCEDURE delete_task_group(IN tgk int)
+BEGIN
+    SET SQL_SAFE_UPDATES = 0;
+
+    -- Create temporary table with all tasks we will have to delete before deleting the group
+    CREATE TEMPORARY TABLE tasks_to_delete ENGINE=MEMORY SELECT task_key FROM task_group_member WHERE task_group_key = tgk;
+    
+    -- First delete the task group members
+    DELETE tgm
+    FROM task_group_member tgm
+    INNER JOIN tasks_to_delete ttd
+        ON tgm.task_key = ttd.task_key;
+
+    -- Then delete the tasks
+    DELETE t
+    FROM task t
+    INNER JOIN tasks_to_delete ttd
+        ON t.task_key = ttd.task_key;
+
+    -- Now that all child tasks have been deleted, delete the group
+    DELETE FROM task_group where task_group_key = tgk;
+
+    -- Task cleanup
+    DROP TABLE tasks_to_delete;
+    SET SQL_SAFE_UPDATES = 1;
+END //
+DELIMITER ;
